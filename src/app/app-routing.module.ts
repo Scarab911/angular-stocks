a33dd1f36054b5a0125e159a80ffdaf9da2071e3
Paulius B <paulspremium@gmail.com>
2021-11-09 19:44:19 +0200

    login/register componets html and styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddStockFormComponent } from './components/add-stock-form/add-stock-form.component';
 import { EditStockFormComponent } from './components/edit-stock-form/edit-stock-form.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 import { StockInfoComponent } from './components/stock-info/stock-info.component';
 import { StocksListComponent } from './components/stocks-list/stocks-list.component';
 
@@ -9,6 +11,14 @@ const routes: Routes = [
   {
     path: 'home',
     component: StocksListComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   },
   {
     path: 'addStock',
