413d4aa83c7c450a27d70527d2082ab34c2dd038
Paulius B <paulspremium@gmail.com>
2021-11-11 17:55:45 +0200

    change hidden directive to ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-stocks';
-  public isShowForm:boolean = true;
+  public isShowForm:boolean = false;
 }
 
